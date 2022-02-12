217807abcd84ea34a475354c4e2ab03a4cdae1d5
pankajpj07 <pankaj12.pj88@gmail.com>
2022-02-12 18:18:56 +0530

    portfolio first commit

---
+++ src/data/aboutData.js
@@ -0,0 +1,8 @@
+export const aboutData = {
+  title: "Who I am",
+  description1:
+    "My name's Pankaj Jain. I'm a JavaScript developer based in Delhi, India.",
+  description2:
+    "During the day I work as a lead developer at an E-commerce giant and in the evening I help people on discord and utilize the time to built my own products. I spent my leisure hours writing articles and poetry. Right now I'm also trying a hand at Backend. I love to learn and explore new arenas.",
+  image: 2,
+};
---
+++ src/data/headerData.js
@@ -0,0 +1,10 @@
+import resume from "../assets/pdf/resume.pdf";
+
+export const headerData = {
+  name: "Pankaj Jain",
+  title: "JavaScript Developer",
+  desciption:
+    "A thinker by profession and a coding Lover by heart. An ambitious individual with a desire to succeed. A Football fanatic.",
+  image: "https://avatars.githubusercontent.com/u/47389183?v=4",
+  resumePdf: resume,
+};
