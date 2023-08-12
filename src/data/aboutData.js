5e87212886fab432400ba5ac3006e90fb2631904
Pankaj Jain <pankaj12.pj88@gmail.com>
2023-08-12 17:50:37 +0530

    updated resume and changed the description

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,8 +1,8 @@
 export const aboutData = {
   title: "Who I am",
   description1:
-    "My name's Pankaj Jain. I'm a JavaScript developer based in Delhi, India.",
+    "My name's Pankaj Jain. I'm a Mern Stack developer based in Delhi, India.",
   description2:
-    "During the day I work as a front end developer at an E-commerce giant and in the evening I help people on discord and utilize the time to build my own projects. I spend my leisure hours writing code and exploring new technologies. I love to learn and explore new arenas.",
+    "During the day I work as a developer and in the evening I help people on discord and also manage time to build my own projects. I spend my leisure hours writing code and exploring new technologies. I love to learn and explore new arenas.",
   image: 1,
 };
